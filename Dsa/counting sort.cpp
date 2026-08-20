35cd3407156cc5f1ab03c77d61bd8cd8290ca8ff
Md. Sakib Hossaine <116342600+Sakib-Hossaine@users.noreply.github.com>
2026-08-20 18:23:27 +0600

    Add counting sort implementation in C++
    Implemented a simple counting sort algorithm to sort an array of integers. Added a main function to demonstrate sorting functionality.

---
+++ Dsa/counting sort.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h
+using namespace std;
+
+#define MAX_SIZE 100
+#define MAX_RANGE 100
+//https://chat.deepseek.com/share/4itpux5phu71afkyxv
+void countsortSimple(int arr[], int n) {
+    // Find maximum
+    int maxval = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maxval) {
+            maxval = arr[i];
+        }
+    }
+    
+    // Count array
+    int cntArr[MAX_RANGE];
+    for (int i = 0; i <= maxval; i++) {
+        cntArr[i] = 0;
+    }
+    
+    // Count frequencies
+    for (int i = 0; i < n; i++) {
+        cntArr[arr[i]]++;
+    }
+    
+    // Reconstruct sorted array
+    int index = 0;
+    for (int i = 0; i <= maxval; i++) {
+        for (int j = 0; j < cntArr[i]; j++) {
+            arr[index] = i;
+            index++;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {2, 5, 3, 0, 2, 3, 0, 3};
+    int n = 8;
+    cout<<"Original: ";
+    
+    for (int i = 0; i < n; i++) {
+      cout<<arr[i];
+       
+    }
+    cout<<"\n";
+    
+    countsortSimple(arr, n);
+    
+cout<<"Sorted: ";
+    for (int i = 0; i < n; i++) {
+          cout<<arr[i];
+    }
+    cout<<"\n";
+    
+    return 0;
+}
